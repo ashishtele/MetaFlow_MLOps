cbeecd26a323b99e0b5ce390a03baf134875bafd
Ashish Tele <sale.ashish.tele@gmail.com>
2021-12-29 13:22:43 -0500

    Preprocessing transformers added

---
+++ src/data/preprocessors_bonus.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class MeanImputer(BaseEstimator, TransformerMixin):
+    """Numerical missing value imputer."""
+
+    def __init__(self, variables):
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist mean values in a dictionary
+        self.imputer_dict_ = X[self.variables].mean().to_dict()
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature],
+                              inplace=True)
+        return X
+
+
+
+class RareLabelCategoricalEncoder(BaseEstimator, TransformerMixin):
+    """Groups infrequent categories into a single string"""
+
+    def __init__(self, tol=0.05, variables):
+
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.tol = tol
+        self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist frequent labels in dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            # the encoder will learn the most frequent categories
+            t = pd.Series(X[var].value_counts(normalize=True) 
+            # frequent labels:
+            self.encoder_dict_[var] = list(t[t >= self.tol].index)
+
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(
+                X[feature].isin(self.encoder_dict_[feature]),
+                                X[feature], "Rare")
+
+        return X
+
+
+class CategoricalEncoder(BaseEstimator, TransformerMixin):
+    """String to numbers categorical encoder."""
+
+    def __init__(self, variables):
+
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.variables = variables
+
+    def fit(self, X, y):
+        temp = pd.concat([X, y], axis=1)
+        temp.columns = list(X.columns) + ["target"]
+
+        # persist transforming dictionary
+        self.encoder_dict_ = {}
+
+        for var in self.variables:
+            t = temp.groupby([var])["target"].mean().sort_values(ascending=True).index
+            self.encoder_dict_[var] = {k: i for i, k in enumerate(t, 0)}
+
+        return self
+
+    def transform(self, X):
+        # encode labels
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.encoder_dict_[feature])
+
+        return X
+
+class TemporalVariableTransformer(BaseEstimator, TransformerMixin):
+    	# Temporal elapsed time transformer
+
+    def __init__(self, variables, reference_variable):
+        
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+        
+        self.variables = variables
+        self.reference_variable = reference_variable
+
+    def fit(self, X, y=None):
+        # we need this step to fit the sklearn pipeline
+        return self
+
+    def transform(self, X):
+
+    	# so that we do not over-write the original dataframe
+        X = X.copy()
+        
+        for feature in self.variables:
+            X[feature] = X[self.reference_variable] - X[feature]
+
+        return X
+
+
+
+# categorical missing value imputer
+class Mapper(BaseEstimator, TransformerMixin):
+
+    def __init__(self, variables, mappings):
+
+        if not isinstance(variables, list):
+            raise ValueError('variables should be a list')
+
+        self.variables = variables
+        self.mappings = mappings
+
+    def fit(self, X, y=None):
+        # we need the fit statement to accomodate the sklearn pipeline
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].map(self.mappings)
+
+        return X
